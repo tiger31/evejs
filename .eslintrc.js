98f31ba824b07be0017bb7af45b70c3ac1d7011f
Ilya Sergeev <tiger31839@gmail.com>
2019-11-16 00:36:25 +0300

    ESLint config changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,37 @@ module.exports = {
 			'error',
 			'single'
 		],
+		'no-param-reassign': 2,
+		'prefer-promise-reject-errors': 2,
+		'no-undefined': 2,
+		'callback-return': 2,
+		'handle-callback-err': 2,
+		'array-bracket-spacing': [
+			'error',
+			'always',
+		],
+		'max-lines-per-function': [
+			'warn',
+			{ max: 30, skipComments: true }
+		],
+		'max-nested-callbacks': [
+			'warn',
+			3
+		],
+		'no-lonely-if': 2,
+		'no-multiple-empty-lines': 2,
+		'object-curly-spacing': 2,
+		'arrow-parens': [
+			"error", "as-needed", { "requireForBlockBody": true }
+		],
+		'arrow-spacing': [
+			"error", { before: true, after: true }
+		],
+		'no-useless-rename': 2,
+		'no-var': 2,
+		'prefer-arrow-callback': 2,
+		'prefer-const': 2,
+		'prefer-template': 2,
 		'babel/semi': 1
 	}
 };
