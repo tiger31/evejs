5b793d68f63fba3cedc2753a246482ee9fa6fb56
Ilya Sergeev <tiger31839@gmail.com>
2019-11-21 21:57:14 +0300

    Runner files search moved to glob

--- test/Runner/Common.test.js
+++ test/Runner/Common.test.js
@@ -2,20 +2,20 @@ const Runner = require('../../lib/classes/Runner');
 
 describe('Runner (Common)', () => {
 	it('Create runner', () => {
-		const runner = new Runner();
+		const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
 	});
 	describe('Exports', () => {
 		it('Add suite', () => {
-			const runner = new Runner();
-			runner.fn = () => { mi('Suite', () => {})};
+			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
+			runner.mi('Suite', () => {}, {});
 			chai.expect(runner.run).to.not.throw();
 			chai.expect(runner.MISuites).to.have.lengthOf(1);
 			chai.expect(runner.MISuites).to.include.property('0')
 				.and.to.include.property('name', 'Suite')
 		});
 		it('Add seed', () => {
-			const runner = new Runner();
-			runner.fn = () => { seed('Seed', (context) => { context.a = 1 }) }
+			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
+			runner.seed('Seed', (context) => { context.a = 1 });
 			chai.expect(runner.run).to.not.throw();
 			chai.expect(runner.seeds).to.have.lengthOf(1);
 			chai.expect(runner.seeds).to.include.property('0')
--- test/Runner/filesIncluding/Including.test.js
+++ test/Runner/filesIncluding/Including.test.js
@@ -5,7 +5,7 @@ const Test = require('../../../lib/classes/Test');
 
 describe('Runner files including', () => {
 	it('Including single file', () => {
-		const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.mi.js" }});
+		const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', pattern: "*.mi.js" }});
 		chai.expect(runner.MISuites).to.have.lengthOf(1);
 		chai.expect(runner.MISuites).to.include.property('0')
 			.and.to.be.instanceOf(Suite)						//Suite
