a5b9b3fccd9dbc31f9e54da0be70d9c9e822cd0b
Ilya Sergeev <tiger31839@gmail.com>
2019-12-09 18:27:55 +0300

    Fixed bug appered because of globstar

--- test/Runner/Common.test.js
+++ test/Runner/Common.test.js
@@ -2,18 +2,18 @@ const Runner = require('../../lib/classes/Runner');
 
 describe('Runner (Common)', () => {
 	it('Create runner', () => {
-		const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
+		const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', pattern: "*.test.js" }});
 	});
 	describe('Exports', () => {
 		it('Add suite', () => {
-			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
+			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', pattern: "*.test.js" }});
 			runner.mi('Suite', () => {}, {});
 			chai.expect(runner.MISuites).to.have.lengthOf(1);
 			chai.expect(runner.MISuites).to.include.property('0')
 				.and.to.include.property('name', 'Suite')
 		});
 		it('Add seed', () => {
-			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', patter: "*.test.js" }});
+			const runner = new Runner({ test: { dir: 'test/Runner/filesIncluding/tests', pattern: "*.test.js" }});
 			runner.seed('Seed', (context) => { context.a = 1 });
 			chai.expect(runner.seeds).to.have.lengthOf(1);
 			chai.expect(runner.seeds).to.include.property('0')
