fc7644ce219dc09594b5e109a32db62496a7deec
Ilya Sergeev <tiger31839@gmail.com>
2020-02-04 21:44:39 +0300

    WIP: Driver. Runnables manager, bluebirds integration. Need tests for TestSuite class

--- test/TestSuite.test.js
+++ test/TestSuite.test.js
@@ -1,4 +1,5 @@
 const TestSuite = require('../lib/classes/TestSuite');
+const Test = require('../lib/classes/Test');
 
 
 describe("TestSuite", () => {
@@ -16,7 +17,6 @@ describe("TestSuite", () => {
 			filter: (scope, parent, fn) => fn(),
 		}});
 		chai.expect(s.errors).to.be.empty;
-		chai.expect(s.runnables).to.have.lengthOf(2);
 		chai.expect(s.suites).to.have.lengthOf(1);
 		chai.expect(s.suites).to.include.property('0')
 			.and.include.property('name', 'Suite');
@@ -24,4 +24,45 @@ describe("TestSuite", () => {
 		chai.expect(s.tests).to.include.property('0')
 			.and.include.property('name', 'Test');
 	});
+	it('Run single test', async () => {
+		const s = new TestSuite({fn: () => {
+				test("Test", () => {});
+			}, name: "TestSuite", context: {}, runner: {
+				filter: (scope, parent, fn) => fn(),
+		}});
+		s.run();
+		await chai.expect(new Promise((rs, rj) => {
+			s.run().then(rs).catch(rj);
+		})).to.not.be.rejected;
+		chai.expect(s.tests[0].state).to.be.equal(Test.states.PASSED);
+	});
+	it('Hook sequence', async () => {
+		const arr = [];
+		const s = new TestSuite({fn: () => {
+				before(() => { arr.push('b'); });
+				beforeEach(() => { arr.push('be'); });
+				after(() => { arr.push('a'); });
+				afterEach(() => { arr.push('ae'); });
+				test("Test", () => { arr.push('t') });
+			}, name: "TestSuite", context: {}, runner: {
+				filter: (scope, parent, fn) => fn(),
+		}});
+		await chai.expect(new Promise((rs, rj) => {
+			s.run().then(rs).catch(rj);
+		})).to.not.be.rejected;
+		chai.expect(arr).to.have.members(['b', 'be', 't', 'ae', 'a']);
+	});
+	it('Skip flag', async () => {
+		const s = new TestSuite({fn: () => {
+				test('Failed', () => { throw new Error('error') }, { step: true });
+				test("Test", () => {});
+			}, name: "TestSuite", context: {}, runner: {
+				filter: (scope, parent, fn) => fn(),
+			}});
+		await chai.expect(new Promise((rs, rj) => {
+			s.run().then(rs).catch(rj);
+		})).to.not.be.rejected;
+		chai.expect(s.tests).to.include.property('1')
+			.and.include.property('state', Test.states.PENDING);
+	})
 });
